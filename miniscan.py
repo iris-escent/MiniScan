71fdaffcdb1c50de54e15144ef1db521410577e4
iridescent <604854259@qq.com>
2026-08-13 15:18:40 +0800

    scan_port封装为函数

--- miniscan.py
+++ miniscan.py
@@ -1,21 +1,27 @@
 import socket
+import errno
+import os
 
+def scan_port(host,port):
 
-host = "127.0.0.1"
-port = 8000
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    #使用socket 模块的 socket 类，使用 IPv4 地址，TCP 协议
+    sock.settimeout(1) 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-#使用socket 模块的 socket 类，使用 IPv4 地址，TCP 协议
-sock.settimeout(1) 
+    result = sock.connect_ex((host, port)) 
+    #非阻塞式的连接函数，尝试连接远程服务器 TCP三次握手,错误返回错误码
 
-result = sock.connect_ex((host, port)) 
-#非阻塞式的连接函数，尝试连接远程服务器 TCP三次握手
+    if result == 0:
+        print(f"[+] {host}:{port} open")
+    else:
+        error_name = errno.errorcode.get(result,"unknow")
+        error_msg = os.strerror(result)
 
-if result == 0:
-    print(f"[+] {host}:{port} open")
-else:
-    print(f"[-] {host}:{port} closed")
+        print(
+            f"[-] {host}:{port} not open,"  #这样换行
+            f"code={result}, {error_name}:{error_msg}"
+            )
 
-sock.close()
+    sock.close()
 
-
+scan_port("127.0.0.1", 80)
